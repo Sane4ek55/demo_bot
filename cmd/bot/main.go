5f6915936ee7bf9336b14b9e19dbf14c41384256
Martynov Aleksandr Andreevich <martynovaleks@ozon.ru>
2023-09-19 16:41:08 +0300

    Extract TOKEN to .env

---
+++ cmd/bot/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func main() {
+	godotenv.Load()
+	token := os.Getenv("TOKEN")
+
+	//myTokenBot := "6095770616:AAF6CjZG-QGASyS9LwWwT7SiXNi2s4WiOE8"
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.UpdateConfig{
+		Timeout: 60,
+	}
+
+	updates := bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Your wrote: "+update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Sane4ek55/demo_bot
 
 go 1.21rc4
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/joho/godotenv v1.5.1
+)
